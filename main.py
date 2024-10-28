1c53047ef225c890ba5d83e860c21c729c03c85c
devnsko <20denysko05@gmail.com>
2024-10-28 03:42:14 +0100

    gcp fix

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ from core.handlers.admin import admin
 from core.database.models import async_main
 from core.settings import settings
 
+# Temporary solution: "Google Cloud Run" need to pass Health Check
+# So we will add single healthcheck endpoint and will figure out more interesting solution later
+from aiohttp import web
+
+# Health check route handler
+async def health(request):
+    return web.Response(text="Bot is running!")
+
+# Function to set up and start the aiohttp web server
+async def start_aiohttp_server():
+    app = web.Application()
+    app.router.add_get('/health', health)  # Add the health check route
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host="0.0.0.0", port=8080)
+    await site.start()
+
 
 async def main():
     logging.basicConfig(level=logging.INFO,
@@ -21,7 +38,12 @@ async def main():
     ))
     dp = Dispatcher()
     dp.include_routers(user, admin)
-    await dp.start_polling(bot)
+    
+    # Start both the bot polling and aiohttp server concurrently
+    await asyncio.gather(
+        start_aiohttp_server(),  # Start aiohttp server for health checks
+        dp.start_polling(bot)    # Start bot polling
+    )
 
 
 if __name__ == '__main__':
